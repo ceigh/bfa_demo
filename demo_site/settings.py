e79ee60f44f208167cfee7dc8326c1926cc00428
ceigh <ceigh@pm.me>
2019-01-02 17:17:48 +0500

    feat: Add favicon

--- demo_site/settings.py
+++ demo_site/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# No valid
 SECRET_KEY = 'se+3og&@0t=a#eapx2-+hl#onfv7lsfl&fxpw(z@s(!6y(j9h)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'bfa.pythonanywhere.com',
-                 '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
